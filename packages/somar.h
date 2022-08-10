c36ef181c2b9892bad10508a2d19411848589f6e
silvadias <luisdias04@hotmail.com>
2022-08-10 20:14:48 -0300

    Concluido arquivo de biblioteca somar.h para conferir se as mudanças refletem no container após edição.

---
+++ packages/somar.h
@@ -0,0 +1,10 @@
+#ifndef SOMA_H_INCLUDED
+#define SOMA_H_INCLUDED
+
+float somar ( float valor_1, float Valor_2 ){
+    float resultado;
+    resultado = valor_1 + Valor_2;
+    return resultado;
+}
+
+#endif
